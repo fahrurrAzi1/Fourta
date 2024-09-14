dec900af4515f24ea91b3ba9368cef3e70acf318
fahrurrAzi1 <moba8630@gmail.com>
2024-09-14 01:38:03 -0700

    menambahkan konfigurasi untuk vite js 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     
     <!-- skrip untuk vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
