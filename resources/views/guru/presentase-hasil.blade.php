84c5cdac959de3a933e33942ba944902a6dae33a
fahrurrAzi1 <moba8630@gmail.com>
2024-09-14 19:21:25 -0700

    update untuk ekspor halaman presentase dan hasil siswa terbaru

--- resources/views/guru/presentase-hasil.blade.php
+++ resources/views/guru/presentase-hasil.blade.php
@@ -74,6 +74,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
 
     <script>
+        // alternatif kode
+        
         // $(document).ready(function() {
 
         //     var selectedJenis = '';
