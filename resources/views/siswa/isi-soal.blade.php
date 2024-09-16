2d8cf53b3ca1412a42c0318a39831fa943a8e8bb
fahrurrAzi1 <moba8630@gmail.com>
2024-09-15 21:32:40 -0700

    update untuk jawab soal perbaikan error

--- resources/views/siswa/isi-soal.blade.php
+++ resources/views/siswa/isi-soal.blade.php
@@ -2,7 +2,7 @@
 
     @section('header')
         <title>Jawab Pertanyaan</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @endsection
 
@@ -180,7 +180,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
 
     <!-- skrip untuk bootstrsap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
 
